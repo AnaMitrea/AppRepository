f43aa006564d3c9590ee3cea7f470534c682a329
Ana Maria Mitrea <72494328+AnaMitrea@users.noreply.github.com>
2021-12-04 19:50:42 +0200

    Update client.c

--- Baza la server+client/client.c
+++ Baza la server+client/client.c
@@ -11,43 +11,43 @@
 
 extern int errno; //codul de eroare returnat de anumite apeluri
 
-int port; // portul de conectare la server
+int port; 
 
 int main (int argc, char *argv[])
 {
-  int sd;			// descriptorul de socket
+  int sd;			// descriptor socket
   struct sockaddr_in server;	// structura folosita pentru conectare 
-  char msg[100];		// mesajul trimis
+  char command[100];		// comanda introdusa
 
-  /* exista toate argumentele in linia de comanda? */
+  /* Verificare existenta argumente in linia de comanda */
   if (argc != 3)
     {
       printf ("Sintaxa: %s <adresa_server> <port>\n", argv[0]);
       return -1;
     }
 
-  /* stabilim portul */
+  /* Stabilire port */
   port = atoi (argv[2]);
 
-  /* cream socketul */
+  /* Creare socket */
   if ((sd = socket (AF_INET, SOCK_STREAM, 0)) == -1)
     {
-      perror ("Eroare la socket().\n");
+      perror ("[client] Error socket().\n");
       return errno;
     }
 
-  /* umplem structura folosita pentru realizarea conexiunii cu serverul */
-  /* familia socket-ului */
+  /* Umplere structura folosita pt realizarea conexiunii cu serverul */
+  /* Familia socket-ului */
   server.sin_family = AF_INET;
-  /* adresa IP a serverului */
+  /* Adresa IP a serverului */
   server.sin_addr.s_addr = inet_addr(argv[1]);
-  /* portul de conectare */
+  /* Portul de conectare */
   server.sin_port = htons (port);
   
-  /* ne conectam la server */
+  /* Conectare client la server */
   if (connect (sd, (struct sockaddr *) &server,sizeof (struct sockaddr)) == -1)
     {
-      perror ("[client]Eroare la connect().\n");
+      perror ("[client] Error connect().\n");
       return errno;
     }
 
@@ -60,47 +60,84 @@ int main (int argc, char *argv[])
 
   while(1)
   {
-    /* citirea mesajului */
-    bzero (msg, 100);
+    /* Citire comanda introdusa */
+    bzero (command, 100);
     printf ("[client] Write your command: ");
     fflush (stdout);
-    read (0, msg, 100);
+    read (0, command, 100);
 
-    msg[strlen(msg)-1]='\0';
+    command[strlen(command)-1]='\0';
 
-    if(strcmp(msg,"Disconnect") == 0)
+    // Trimitere comanda introdusa catre server
+    if(strcmp(command,"Disconnect") == 0)
     {
-        /* trimiterea mesajului la server */
-      if (write (sd, "Disconnect", strlen("Disconnect")) <= 0)
+      /// trimitere bytes la server 
+      int bytes = strlen("Disconnect");
+
+      if (write (sd, &bytes, sizeof(int)) <= 0)
       {
-        perror ("[client] Error at writting message for server.\n");
-        return errno;
+          perror ("[client] Error at writting num bytes for server.\n");
+          return errno;
+      }
+
+      sleep(1);
+
+      // trimitere comanda la server
+      if (write (sd, command, bytes) <= 0)
+      {
+          perror ("[client] Error at writting command for server.\n");
+          return errno;
       }
 
       printf("You have disconnected from the server...\n");
-      break;
+
+      close(sd);
+      return 0;
     }
     else
-    if(strcmp(msg,"Insert") == 0)
+    if(strcmp(command,"Insert") == 0)
     {
-      if (write (sd, "Insert", strlen("Insert")) <= 0)
+      // trimitere bytes la server
+      int bytes = strlen("Insert");
+
+      if (write (sd, &bytes, sizeof(int)) <= 0)
+      {
+          perror ("[client] Error at writting num bytes for server.\n");
+          return errno;
+      }
+
+      sleep(1);
+
+      // trimitere comanda catre server
+      if (write (sd, command, bytes) <= 0)
+      {
+          perror ("[client] Error at writting command for server.\n");
+          return errno;
+      }
+    }
+    else
+    if(strcmp(command,"Search") == 0)
+    {
+      // trimitere bytes la server
+      int bytes = strlen("Search");
+
+      if (write (sd, &bytes, sizeof(int)) <= 0)
+      {
+          perror ("[client] Error at writting num bytes for server.\n");
+          return errno;
+      }
+
+      sleep(1);
+
+      if (write (sd, command, bytes) <= 0)
       {
         perror ("[client] Error at writting message for server.\n");
         return errno;
       }
     }
     else
-    if(strcmp(msg,"Search") == 0)
     {
-      if (write (sd, "Search", strlen("Search")) <= 0)
-      {
-        perror ("[client] Error at writting message for server.\n");
-        return errno;
-      }
-    }
-    else
-    {
-        printf("%s is an unavailable command. \n", msg);
+        printf("%s is an unavailable command. \n", command);
         printf("Available commands on the server: \n");
         printf("[1] To insert your Application, please write \"Insert\". \n");
         printf("[2] To search an Application, please write \"Search\". \n");
@@ -108,19 +145,29 @@ int main (int argc, char *argv[])
         printf("\n");
     }
 
-
+    int bytes_sent;
+    
     /* citirea raspunsului dat de server (apel blocant pana cand serverul raspunde) */
-    if (read (sd, msg, 100) < 0)
+    if (read (sd, &bytes_sent, sizeof(int)) < 0)
+    {
+      perror ("[client] Error at reading num bytes from server.\n");
+      return errno;
+    }
+
+    sleep(1);
+
+    char information[bytes_sent];
+
+    if (read (sd, information, bytes_sent) < 0)
     {
       perror ("[client] Error at reading message from server.\n");
       return errno;
     }
 
+    information[strlen(information) - 1] = '\0';
+
     /* afisam mesajul primit */
-    printf ("[client] Message: \n * %s\n", msg);
+    printf ("[client] Message: \n * %s\n", information);
 
   }
-
-  /* inchidem conexiunea, am terminat */
-  close (sd);
 }
