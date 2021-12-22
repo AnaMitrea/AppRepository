fb294c3be16df8bd1cc8ccd96e9bab8adbff0e97
Ana Maria Mitrea <72494328+AnaMitrea@users.noreply.github.com>
2021-12-22 15:56:33 +0200

    LIBRARIILE FOLOSITE

---
+++ Baza la server+client/libraries.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
