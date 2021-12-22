78adfe6bc4bf114347df3f4f3a099a41049e0e7d
Ana Maria Mitrea <72494328+AnaMitrea@users.noreply.github.com>
2021-12-22 16:18:20 +0200

    Update client.cpp

--- Baza la server+client/client.cpp
+++ Baza la server+client/client.cpp
@@ -1,92 +1,32 @@
 #include "libraries.h"
-
-#define MAXIMUM 1024
 int port;
-int errorHandling(string errmsg);
-void printInstructions();
-void sendingCommand(int sd, int bytes, string command);
-void readingInfo(int sd);
 
-int errorHandling(string errmsg)
-{
-  perror(errmsg.c_str());
-  return errno;
-}
-
-void printInstructions()
-{
-  printf("\n");
-  printf("Available commands on the server: \n");
-  printf("[1] To insert your Application, please write \"Insert\". \n");
-  printf("[2] To search an Application, please write \"Search\". \n");
-  printf("[3] To Disconnect from the server, please write \"Disconnect\". \n");
-  printf("\n");
-}
-
-void sendingCommand(int sd, int bytes, string command)
-{
-  if (write (sd, &bytes, sizeof(int)) <= 0) /// trimitere bytes la server 
-  {
-    errorHandling("[client] Error at writting num bytes for server.\n");
-  }
-  
-  if (write (sd, command.c_str(), bytes) <= 0)  // trimitere comanda la server
-  {
-    errorHandling("[client] Error at writting command for server.\n");
-  }
-}
-
-void readingInfo(int sd)
-{
-  int bytes_sent;
-  /* citirea raspunsului dat de server (apel blocant pana cand serverul raspunde) */
-  if (read (sd, &bytes_sent, sizeof(int)) < 0)
-  {
-    errorHandling("[client] Error at reading num bytes from server.\n");
-  }
-
-  char information[bytes_sent];
-  bzero(information, bytes_sent);
-
-  if (read (sd, information, bytes_sent) < 0)
-  {
-    errorHandling("[client] Error at reading message from server.\n");
-  }
-
-  information[strlen(information)] = '\0';
-
-  /* afisam mesajul primit */
-  printf ("[client] %s\n", information);
-}
 
 int main (int argc, char *argv[])
 {
-  int sd;			// socket descriptor
-  struct sockaddr_in server;	// structura folosita pentru conectare 
-  char command[MAXIMUM];		// comanda introdusa
+  int sd;
+  struct sockaddr_in server;
+  string command;		// input command
 
-  /* Verificare existenta argumente in linia de comanda */
+  // ./client <server_address> <port>
   if (argc != 3)
   {
-    printf ("Sintaxa: %s <adresa_server> <port>\n", argv[0]);
+    printf ("Sintax: %s <server_address> <port>\n", argv[0]);
     return -1;
   }
 
-  
   port = atoi (argv[2]); /* Stabilire port */
 
-  /* Creare socket */
   if ((sd = socket (AF_INET, SOCK_STREAM, 0)) == -1)
   {
     errorHandling("[client] Error socket().\n");
   }
 
-  /* Umplere structura folosita pt realizarea conexiunii cu serverul */
-  server.sin_family = AF_INET; /* Familia socket-ului */
-  server.sin_addr.s_addr = inet_addr(argv[1]); /* Adresa IP a serverului */
-  server.sin_port = htons (port); /* Portul de conectare */
+  // Filling the structure used for establishing connection with server
+  server.sin_family = AF_INET; // Socket Family
+  server.sin_addr.s_addr = inet_addr(argv[1]); // IP Address
+  server.sin_port = htons (port); // Connection port
   
-  /* Conectare client la server */
   if (connect (sd, (struct sockaddr *) &server,sizeof (struct sockaddr)) == -1)
   {
     errorHandling("[client] Error connect().\n");
@@ -97,46 +37,45 @@ int main (int argc, char *argv[])
   while(1)
   {
     int ok = 1;
-    /* Citire comanda introdusa */
-    bzero (command, MAXIMUM);
-    printf ("[client] Write your command: ");
-    fflush (stdout);
-    read (0, command, MAXIMUM);
 
-    command[strlen(command) - 1] = '\0';
+    command.clear();
+
+    cout << "[client] Write your command: ";
+    fflush (stdout);
+    getline(cin, command);
 
     // Trimitere comanda introdusa catre server
-    if(strcmp(command,"Disconnect") == 0)
+    if(command == "Disconnect")
     {
       int bytes = strlen("Disconnect") + 1;
-      sendingCommand(sd, bytes, command);
+      sendingCommand_IN_CLIENT(sd, bytes, command);
 
-      printf("You have disconnected from the server...\n");
+      cout << "You have disconnected from the server...\n"s;
       close(sd);
       return 0;
     }
     else
-    if(strcmp(command,"Insert") == 0)
+    if(command == "Insert")
     {
       int bytes = strlen("Insert") + 1;
-      sendingCommand(sd, bytes, command);
+      sendingCommand_IN_CLIENT(sd, bytes, command);
     }
     else
-    if(strcmp(command,"Search") == 0)
+    if(command == "Search")
     {
       int bytes = strlen("Search") + 1;
-      sendingCommand(sd, bytes, command);
+      sendingCommand_IN_CLIENT(sd, bytes, command);
     }
     else
     {
-        printf("\n%s is an unavailable command. \n", command);
+        cout << endl << command << "is an unavailable command. \n";
         printInstructions();
         ok = 0;
     }
 
     if(ok == 1)
     {
-      readingInfo(sd);
+      readingInfo_IN_CLIENT(sd);
     }
   }
 }
